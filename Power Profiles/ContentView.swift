9ff39db9c190d435a0850706d943667a47d2cc0a
Jesse Crocker <w0291240@nscc.ca>
2026-05-26 23:30:02 -0300

    Initial Commit

---
+++ Power Profiles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Power Profiles
+//
+//  Created by Jesse Crocker on 2026-05-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Power Profiles/Power_ProfilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Power_ProfilesApp.swift
+//  Power Profiles
+//
+//  Created by Jesse Crocker on 2026-05-26.
+//
+
+import SwiftUI
+
+@main
+struct Power_ProfilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
