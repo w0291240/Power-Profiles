e5f3bd5e0113d1e4136b83f9906469394a4f66ef
Jesse Crocker <w0291240@nscc.ca>
2026-05-27 01:58:42 -0300

    Frontend/ UI work in progress

---
+++ Power Profiles/AdapterView.swift
@@ -0,0 +1,18 @@
+//
+//  AdapterView.swift
+//  Power Profiles
+//
+//  Created by Jesse Crocker on 2026-05-27.
+//
+
+import SwiftUI
+
+struct AdapterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdapterView()
+}
---
+++ Power Profiles/BatteryView.swift
@@ -0,0 +1,18 @@
+//
+//  BatteryView.swift
+//  Power Profiles
+//
+//  Created by Jesse Crocker on 2026-05-27.
+//
+
+import SwiftUI
+
+struct BatteryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BatteryView()
+}
